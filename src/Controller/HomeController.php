6696ffba3f34099ca344c744e497f05b0811e2ca
Florian <mre.florian@gmail.com>
2022-10-22 15:49:07 +0200

    Mise en place de Doctrine et de nos premières entitées

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\ProductRepository;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
@@ -10,7 +11,12 @@ class HomeController extends AbstractController {
   /**
    *  @Route("/", name="homepage")
    */
-  public function homePage() {
-    return $this->render('home.html.twig');
+  public function homePage(ProductRepository $productRepository) {
+
+    $product = $productRepository->findBy([], [], 3);
+
+    dump($product);
+    
+    return $this->render('home.html.twig', ['products' => $product]);
   }
 }
